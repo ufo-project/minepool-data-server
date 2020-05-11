b5219d84fecfb30ac0bc6b2baebf4152df3a7481
sankim1789 <sankim1789@outlook.com>
2020-05-11 13:15:40 +0800

    add minerstat24h

--- bp.py
+++ bp.py
@@ -6,12 +6,13 @@ from flask import Blueprint
 import time
 import json
 from utils import get_format_datetime
-from model import TblStatInfoTotal30m
+from model import TblStatInfoTotal30m, TblStatInfoDetail30m
 from difficulty import UfoDiff
 from stat_info import TotalStatInfo30Min, DetailStatInfo30Min
+from peewee import fn
 
 
-bp = Blueprint('', __name__, url_prefix='/')
+bp = Blueprint('', __name__, url_prefix='/v2/')
 
 
 @bp.route('/poolinfo30m', methods=('GET', 'POST'))
@@ -56,8 +57,8 @@ def poolstat24h():
     for info in infos:
         sharesdiff = float(info.totaldiff)
         hashrate = float(info.hashrate) / 1000 / 1000
-        validcount = info.validcount
-        invalidcount = info.invalidcount
+        validcount = int(info.validcount)
+        invalidcount = int(info.invalidcount)
         periodtime = str(info.periodtime)
         stat_24h_list.append(
             {
@@ -100,7 +101,63 @@ def minerinfo30m(query_name):
     })
 
 
+@bp.route('/minerstat24h/<query_name>', methods=('GET', 'POST'))
+def minerstat24h(query_name):
+    t = int(time.time()) - 86400
+    s = get_format_datetime(t)
 
+    infos = []
+    for i in range(3):
+        if i == 0:
+            if '.' not in query_name:
+                continue
+            l = query_name.split('.', 1)
+            infos = TblStatInfoDetail30m.select(fn.SUM(TblStatInfoDetail30m.totaldiff).alias('totaldiffsum'),
+                                                fn.SUM(TblStatInfoDetail30m.validcount).alias('validcountsum'),
+                                                fn.SUM(TblStatInfoDetail30m.invalidcount).alias('invalidcountsum'),
+                                                TblStatInfoDetail30m.periodtime)\
+                .where(TblStatInfoDetail30m.periodtime > s, TblStatInfoDetail30m.uname == l[0], TblStatInfoDetail30m.worker == l[1])\
+                .group_by(TblStatInfoDetail30m.periodtime).order_by(TblStatInfoDetail30m.periodtime).execute()
+            if len(infos) > 0:
+                break
+        elif i == 1:
+            infos = TblStatInfoDetail30m.select(fn.SUM(TblStatInfoDetail30m.totaldiff).alias('totaldiffsum'),
+                                                fn.SUM(TblStatInfoDetail30m.validcount).alias('validcountsum'),
+                                                fn.SUM(TblStatInfoDetail30m.invalidcount).alias('invalidcountsum'),
+                                                       TblStatInfoDetail30m.periodtime) \
+                .where(TblStatInfoDetail30m.periodtime > s, TblStatInfoDetail30m.uname == query_name)\
+                .group_by(TblStatInfoDetail30m.periodtime).order_by(TblStatInfoDetail30m.periodtime).execute()
+            infos = [i for i in infos]
+            if len(infos) > 0:
+                break
+        else:
+            infos = TblStatInfoDetail30m.select(fn.SUM(TblStatInfoDetail30m.totaldiff).alias('totaldiffsum'),
+                                                fn.SUM(TblStatInfoDetail30m.validcount).alias('validcountsum'),
+                                                fn.SUM(TblStatInfoDetail30m.invalidcount).alias('invalidcountsum'),
+                                                       TblStatInfoDetail30m.periodtime) \
+                .where(TblStatInfoDetail30m.periodtime > s, TblStatInfoDetail30m.worker == query_name) \
+                .group_by(TblStatInfoDetail30m.periodtime).order_by(TblStatInfoDetail30m.periodtime).execute()
+            infos = [i for i in infos]
+            if len(infos) > 0:
+                break
+
+    stat_24h_list = []
+    for info in infos:
+        sharesdiff = float(info.totaldiffsum)
+        hashrate = float(UfoDiff.get_hash_rate_by_diff(sharesdiff, 1800, None)) / 1000 / 1000
+        validcount = int(info.validcountsum)
+        invalidcount = int(info.invalidcountsum)
+        periodtime = str(info.periodtime)
+        stat_24h_list.append(
+            {
+                "sharesdiff": "%.06f" % sharesdiff,
+                "hashrate": "%.06f" % hashrate,
+                "validcount": validcount,
+                "invalidcount": invalidcount,
+                "periodtime": periodtime
+            }
+        )
+    return json.dumps(stat_24h_list)
 
 
 
